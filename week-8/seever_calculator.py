50020065e26a443c66df05b99e4030d96054b9ee
Jake Seever <jseever@my365.bellevue.edu>
2019-06-20 10:51:50 -0500

    Adding the 8.3 exercise files.

---
+++ week-8/seever_calculator.py
@@ -0,0 +1,22 @@
+#============================================
+# Title:  seever_calculator.py
+# Author: Jake Seever
+# Date:   20 June 2019
+# Description: Python calculations example. 
+#============================================
+
+def add(number1, number2): #function to add two numbers and return the added total.
+    return number1 + number2
+
+def subtract(number1, number2): #function to subtract two numbers and return the subtracted total.
+    return number1 - number2
+
+def divide(number1, number2): #function to divide two numbers and return the divided total.
+    return number1 / number2
+
+# Print the result of each calculator call
+print(add(1,2))
+
+print(subtract(4,1))
+
+print(divide(8,2))
