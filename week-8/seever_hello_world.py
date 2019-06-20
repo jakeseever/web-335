478fc98ac250e5e8d5da3ebba5ebdbc0101be6d9
Jake Seever <jseever@my365.bellevue.edu>
2019-06-20 10:22:41 -0500

    Adding attribution to the 8.2 exercise files.

--- week-8/seever_hello_world.py
+++ week-8/seever_hello_world.py
@@ -1,1 +1,8 @@
+#============================================
+# Title:  seever_hello_world.py
+# Author: Jake Seever
+# Date:   20 June 2019
+# Description: 'Hello World' first python example.
+#============================================
+
 print('Hello World')
